6f74db7b25aa6cdf4e461a86ce02523a0e78b76b
qideyin <qdeyin@163.com>
2023-12-20 14:57:32 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // 修改浏览器标签页中内容
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header 设置头部固定
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar 设置左侧导航 头部是否显示logo
+   */
+  sidebarLogo: false
+}
